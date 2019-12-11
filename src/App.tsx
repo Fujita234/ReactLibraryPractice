20c4e81fc30aa9947bb24a2069f3d04eef14276b
Fujita234 <exia.hinata0622@gmail.com>
2019-12-11 15:29:17 +0900

    コンポーネントについて触ってます

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CharacterList, { Character } from './CharacterList';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.tsx</code> World.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const charactors: Character[] = [
+      {
+        id: 1,
+        name: 'らぁら',
+        age: 10,
+      },
+      {
+        id: 2,
+        name: 'みれぃ',
+        age: 13,
+      },
+      {
+        id: 3,
+        name: 'ソフィ',
+        age: 14,
+      },
+    ];
+    return (
+      <div className="container">
+        <header>
+          <h1>プリパラ</h1>
+        </header>
+        <CharacterList school="パプリカ学園" characters={charactors} />
+      </div>
+    );
+  }
 }
 
 export default App;
